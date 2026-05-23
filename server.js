e4ee00fa8aed0325b2d62f770b394bcf586c54e2
Andrew McKnight <thedonk@gmail.com>
2026-05-22 23:05:05 -0700

    feat: install button, maskable icon, dual touch ghost (local + server echo)

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const MANIFEST = JSON.stringify({
   start_url: '/',
   theme_color: '#04060d',
   background_color: '#04060d',
-  icons: [{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml' }],
+  icons: [{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'maskable' }],
 });
 
 const ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
